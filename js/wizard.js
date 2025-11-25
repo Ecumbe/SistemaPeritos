f4894e32b91f1d27c4ac14fa21fe59d60d2419d8
Ecumbe <eduardo.cumbev@ug.edu.ec>
2025-11-25 16:47:42 -0500

    Update wizard.js

--- js/wizard.js
+++ js/wizard.js
@@ -180,7 +180,7 @@
       buttonList: toolbarOptions,
       mode: 'classic',
       lang: (window.SUNEDITOR_LANG && window.SUNEDITOR_LANG['es']) ? window.SUNEDITOR_LANG['es'] : undefined,
-     //- iframeAttributes: { style: 'background-color: #1a202c; color: #e2e8f0; font-family: Arial, sans-serif; font-size: 13.5px;' },---
+      iframeAttributes: { style: 'background-color: #ffffff; color: #111111; font-family: Arial, sans-serif; font-size: 13.5px;' },
       icons: {
         bold: '<i class="fa-solid fa-bold"></i>',
         underline: '<i class="fa-solid fa-underline"></i>',
@@ -1514,4 +1514,5 @@ window.cargarAgendamientosDeHoy = function() {
     if(btnHoy) btnHoy.click();
 
 };
+
 
